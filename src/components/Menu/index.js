957071aa6ef8751c85029b1ad08fcbac194b3c68
Igor Henrique de Araujo <ihenrique@uolinc.com>
2020-07-28 16:50:42 -0300

    Adicionando componentes da Home e arquivo de dados para simular backend com os vídeos

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css'
-import ButtonLink from '../ButtonLink'
+import ButtonLink from './components/ButtonLink'
 
 function Menu() {
     return (
         <nav className="Menu">
             <a href="/">
-                <img className="Logo" src={Logo} alt="Logo da Faflix"/>
+                <img className="Logo" src={Logo} alt="Logo da Fatflix"/>
             </a>
             <ButtonLink className="ButtonLink" href="/">
                 Novo Vídeo
