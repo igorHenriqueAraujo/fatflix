3a255fd71fd8aae8baf3519869948c02ec936ac7
Igor Henrique de Araujo <ihenrique@uolinc.com>
2020-07-29 19:17:38 -0300

    Adicionando roteamento com react router dom e estrutura das páginas de videos e categoria

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css'
-import ButtonLink from './components/ButtonLink'
+import Button from '../Button'
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Logo da Fatflix"/>
-            </a>
-            <ButtonLink className="ButtonLink" href="/">
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/videos">
                 Novo Vídeo
-            </ButtonLink>
+            </Button>
         </nav>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Home from './pages/Home'
+import CadastroVideos from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+const Pagina404 = () => (<div>Não Encontrada</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/videos" component={CadastroVideos} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404}/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
